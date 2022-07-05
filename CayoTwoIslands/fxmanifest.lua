0dbc64c488086fc706587dcbf6e1c2d1c32002ad
TayMcKenzieSA <taylorocarroll@gmail.com>
2022-07-05 19:59:33 +1200

    Updated Resource 4.2.2

--- CayoTwoIslands/fxmanifest.lua
+++ CayoTwoIslands/fxmanifest.lua
@@ -10,6 +10,8 @@ description 'Spawns Cayo Perico alongside Los Santos'
 
 this_is_a_map 'yes'
 
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/mads_no_exp_pumps.ytyp' -- Stops petrol pumps from exploding. Credit to MadsL on the FiveM Forums for this.
+
 client_scripts {
     'scripts/twoislands.lua', -- Both Islands Visible At One Time
 
@@ -17,19 +19,21 @@ client_scripts {
 
     'scripts/blips.lua', -- Map Blips
 
-    'scripts/zones.lua', -- Displays all map zones, fixing the 'bug' of showing North Yankton, and instead showing Cayo Perico
+    'scripts/zones.lua', -- Displays all map zones, fixing the 'bug' of showing North Yankton, and instead, showing Cayo Perico
 
     'scripts/peds_config.lua', -- Invincible peds that appear on the island, ie merryweather
 
-    'scripts/main_peds.lua', -- Don't touch
+    'scripts/main_peds.lua', -- Don't touch this
 
     'scripts/static_emitters.lua' -- Disables annoying 'cheering' SFXs from Arena Wars location near North Yankton / Cayo Perico
+
 }
 
 
 
 ---                                                                     ---
----   THIS IS A FREE RESOURCE PROVIDED TO CFXE FORUMS FOR FIVEM USAGE   ---
----        DO NOT REUPLOAD OR MODIFY WITHOUT MY PERMISSION              ---
+---   THIS IS A FREE RESOURCE PROVIDED TO CFXRE FORUMS FOR FIVEM USAGE   ---
+---        DO NOT REUPLOAD OR MODIFY WITHOUT MY PERMISSION               ---
+---        DO NOT CONVERT FOR SINGLE PLAYER USAGE WITHOUT MY PERMISSION  ---
 ---    YOU CAN COMMENT OUT PEDS AND TELEPORTERS IF YOU WISH TO          ---
----                                                                     ---
+---    DO NOT DM ME FOR ONE ON ONE SUPPORT.                             ---
